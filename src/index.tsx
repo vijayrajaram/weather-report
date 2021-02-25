f157c884d87195ac86b29a9db6572d745f1708c2
Vijay Rajaram <vijay.rajaram@sparzen.com>
2021-02-25 12:54:28 -0500

    Weather URL change

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-// https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly&appid=4941f7dfc07be74f5cea37e02e9a53a0
+// https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,minutely&appid=4941f7dfc07be74f5cea37e02e9a53a0
 
 const render = () => {
     ReactDom.render(<div>Hello World</div>, document.getElementById("root"));
